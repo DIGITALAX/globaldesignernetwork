79eb2b5eeafba7845db993cd3918d7e9d39720ac
emmajane1313 <emma.jane@digitalax.xyz>
2025-11-15 05:45:51 -0500

    mainnet

--- src/app/[lang]/account/page.tsx
+++ src/app/[lang]/account/page.tsx
@@ -1,7 +1,9 @@
 import { getDictionary } from "../dictionaries";
+import { tParams } from "../layout";
 import AccountEntry from "@/app/components/Account/modules/AccountEntry";
 
-export default async function Account() {
-  const dict = await (getDictionary as (locale: any) => Promise<any>)("en");
+export default async function Account({ params }: { params: tParams }) {
+  const { lang } = await params;
+  const dict = await (getDictionary as (locale: any) => Promise<any>)(lang);
   return <AccountEntry dict={dict} />;
 }
